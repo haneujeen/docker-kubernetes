a285d55dacd22363dc882964d944053cef8f34d8
haneujeen <eujeenhan@gmail.com>
2023-12-19 22:38:03 +0900

    Update index.js to force crash with status code zero

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const redis = require('redis');
+const process = require('process');
 
 const app = express();
 const client = redis.createClient({
@@ -9,7 +10,9 @@ const client = redis.createClient({
 });
 client.set('visits', 0);
 
+/* Root route handler */
 app.get('/', (req, res) => {
+    process.exit(0);
     client.get('visits', (err, visits) => {
         res.send('Number of visits: ' + visits);
         client.set('visits', parseInt(visits) +1);
